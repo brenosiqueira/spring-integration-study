798f564a83f5eefa785168e3d3bbd9f3797a1ab0
brenosiqueira <breno.siqueira@hotmail.com>
2017-06-28 22:17:10 -0300

    implements directchannel: failover demostrat in with PrintService forcing Exception

--- src/main/java/com/oreilly/integration/PrintService.java
+++ src/main/java/com/oreilly/integration/PrintService.java
@@ -5,6 +5,9 @@ import org.springframework.messaging.Message;
 public class PrintService {
 
 	public void print(Message<String> message) {
-		System.out.println(message.getPayload());
+		// O outro servico deve consumir as mensagens em caso de erro desse
+		// endpoint
+		throw new RuntimeException("This is erro for demostration");
+		// System.out.println(message.getPayload());
 	}
 }
