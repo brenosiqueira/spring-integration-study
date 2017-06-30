6d24ca8e2d6ec84af79c576bca4f7c6ed4e8ee1b
brenosiqueira <breno.siqueira@hotmail.com>
2017-06-29 21:12:23 -0300

    Implements publishSubscribeChannel

--- src/main/java/com/oreilly/integration/PrintService.java
+++ src/main/java/com/oreilly/integration/PrintService.java
@@ -5,9 +5,6 @@ import org.springframework.messaging.Message;
 public class PrintService {
 
 	public void print(Message<String> message) {
-		// O outro servico deve consumir as mensagens em caso de erro desse
-		// endpoint
-		throw new RuntimeException("This is erro for demostration");
-		// System.out.println(message.getPayload());
+		System.out.println(message.getPayload());
 	}
 }
