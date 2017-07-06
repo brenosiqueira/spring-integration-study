c9214d1bbb4ca541b2444983d587f18d781059ab
brenosiqueira <breno.siqueira@hotmail.com>
2017-07-05 21:30:45 -0300

    implements customFilters

--- src/main/java/com/oreilly/integration/NumericPrintService.java
+++
@@ -1,9 +0,0 @@
-package com.oreilly.integration;
-
-import org.springframework.messaging.Message;
-
-public class NumericPrintService {
-	public void print(Message<?> message) {
-		System.out.println("Is a number " +message.getPayload());
-	}
-}
